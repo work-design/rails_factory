8fb1dad72b02a0333552151ea1e4556cd1e6b96d
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-29 23:19:44 +0800

    fix attribute

--- app/models/rails_factory/product_plan.rb
+++ app/models/rails_factory/product_plan.rb
@@ -1,19 +1,21 @@
 module RailsFactory::ProductPlan
   extend ActiveSupport::Concern
+
   included do
-    t.datetime :start_at
-    t.datetime :finish_at
-    t.string :state
-    t.integer :planned_count, default: 0
-    t.integer :produced_count, default: 0
+    attribute :start_at, :datetime
+    attribute :finish_at, :datetime
+    attribute :state, :string
+    attribute :planned_count, :integer, default: 0
+    attribute :produced_count, :integer, default: 0
+
     belongs_to :product
     has_many :product_items
-    
+
     enum state: {
       planned: 'planned',
       producing: 'producing',
       produced: 'produced'
     }
   end
-  
+
 end
