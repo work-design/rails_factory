a74500ba6824ead57b3c6f26bfaee6c222161b1e
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-23 02:19:30 +0800

    trade items with address and customs

--- app/controllers/factory/my/principal_addresses_controller.rb
+++ app/controllers/factory/my/principal_addresses_controller.rb
@@ -1,36 +1,17 @@
 class Factory::My::PrincipalAddressesController < Factory::My::BaseController
-  before_action :set_principal_address, only: [:show, :edit, :update, :destroy]
+  before_action :set_principal_address, only: [:show]
 
   def index
-    @principal_addresses = current_user.principal_addresses.page(params[:page])
+    @addresses = current_user.principal_addresses.page(params[:page])
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    @principal_address.assign_attributes(principal_address_params)
-
-    unless @principal_address.save
-      render :edit, locals: { model: @principal_address }, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @principal_address.destroy
+    @customs = @address.customs
   end
 
   private
   def set_principal_address
-    @principal_address = PrincipalAddress.find(params[:id])
-  end
-
-  def principal_address_params
-    params.fetch(:principal_address, {}).permit(
-    )
+    @address = current_user.principal_addresses.find(params[:id])
   end
 
 end
