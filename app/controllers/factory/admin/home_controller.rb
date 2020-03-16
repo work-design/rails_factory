33daff764fef6effa17d024420e21e383bc0afd4
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-17 01:23:44 +0800

    close icon

--- app/controllers/factory/admin/home_controller.rb
+++ app/controllers/factory/admin/home_controller.rb
@@ -2,7 +2,7 @@ class Factory::Admin::HomeController < Factory::Admin::BaseController
 
   def index
     if current_receiver
-      @notifications = current_receiver.received_notifications.order(read_at: :asc)
+      @notifications = current_receiver.notifications.order(read_at: :asc)
     else
       @notifications = Notification.none
     end
