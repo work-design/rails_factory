f74e9383abb99256657689ab73202ebe280f8f59
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-22 01:39:47 +0800

    gemfile

--- app/controllers/factory/base_controller.rb
+++ app/controllers/factory/base_controller.rb
@@ -1,3 +1,4 @@
 class Factory::BaseController < RailsFactory.config.app_controller.constantize
+  before_action :support_organ
 
 end
--- app/controllers/factory/my/base_controller.rb
+++ app/controllers/factory/my/base_controller.rb
@@ -1,4 +1,5 @@
 class Factory::My::BaseController < RailsFactory.config.my_controller.constantize
   include RailsTrade::MyCart
+  before_action :support_organ
 
 end
