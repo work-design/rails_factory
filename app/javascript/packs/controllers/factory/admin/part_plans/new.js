53398244a57c7f56bce6db871f42d7b971e688af
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-21 00:23:06 +0800

    factory nav

--- app/javascript/packs/controllers/factory/admin/part_plans/new.js
+++
@@ -1,7 +0,0 @@
-$('#part_plan_start_at_4i').dropdown();
-$('#part_plan_start_at_5i').dropdown();
-$('#part_plan_finish_at_4i').dropdown();
-$('#part_plan_finish_at_5i').dropdown();
-
-
-
--- app/javascript/packs/controllers/factory/admin/product_plans/new.js
+++
@@ -1,7 +0,0 @@
-$('#product_plan_start_at_4i').dropdown();
-$('#product_plan_start_at_5i').dropdown();
-$('#product_plan_finish_at_4i').dropdown();
-$('#product_plan_finish_at_5i').dropdown();
-
-
-
