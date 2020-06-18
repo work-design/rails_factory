0eaecd87408c9927a5d24382e7c70964304ee58c
qinmingyuan <mingyuan0715@foxmail.com>
2020-06-18 21:57:09 +0800

    fix good privder ref

--- app/models/rails_factory/organ.rb
+++ app/models/rails_factory/organ.rb
@@ -3,10 +3,10 @@ module RailsFactory::Organ
 
   included do
     belongs_to :area, optional: true
-    
+
     has_many :part_providers
     has_many :parts, through: :part_providers
-    has_many :good_providers, dependent: :delete_all
+    has_many :good_providers, foreign_key: :provider_id, dependent: :delete_all
   end
 
   def name_detail
