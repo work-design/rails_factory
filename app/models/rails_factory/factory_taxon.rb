b642b0d1b97e3143048476dfd113956845541d1b
qinmingyuan <mingyuan0715@foxmail.com>
2020-12-12 22:19:28 +0800

    use product filter organ id

--- app/models/rails_factory/factory_taxon.rb
+++ app/models/rails_factory/factory_taxon.rb
@@ -6,6 +6,8 @@ module RailsFactory::FactoryTaxon
 
     has_many :factory_providers, dependent: :delete_all
     has_many :providers, through: :factory_providers
+    has_many :product_taxons, ->(o) { where(organ_id: o.provider_ids) }, dependent: :nullify
+    has_many :products, through: :product_taxons
   end
 
 end
