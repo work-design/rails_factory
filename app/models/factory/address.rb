24da1ab541d8f4f229df926d28e9a3f0f972c21e
qinmingyuan <mingyuan0715@foxmail.com>
2021-01-26 08:59:51 +0800

    address

--- app/models/factory/address.rb
+++ app/models/factory/address.rb
@@ -1,6 +1,6 @@
 module Factory
   class Address < ApplicationRecord
-    include Profile::Model::Address if defined? RailsProfile
+    include Profiled::Model::Address if defined? RailsProfile
     include Model::Address
   end
 end
