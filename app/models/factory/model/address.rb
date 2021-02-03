86de91bef0d0224775920e3aeddda2594ba918cb
qinmingyuan <mingyuan0715@foxmail.com>
2021-02-03 13:20:08 +0800

    add namespace

--- app/models/factory/model/address.rb
+++ app/models/factory/model/address.rb
@@ -3,8 +3,8 @@ module Factory
     extend ActiveSupport::Concern
 
     included do
-      has_many :trade_items, dependent: :nullify
-      has_many :productions, through: :trade_items, source: :good, source_type: 'Production'
+      has_many :trade_items, class_name: 'Trade::TradeItem', dependent: :nullify
+      has_many :productions, class_name: 'Factory::Production', through: :trade_items, source: :good, source_type: 'Factory::Production'
     end
 
   end
