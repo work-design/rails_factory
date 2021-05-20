bd707c5008efab8611c0dac5d4a08838cc4f875d
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-20 12:40:12 +0800

    factory

---
+++ app/models/factory.rb
@@ -0,0 +1,11 @@
+module Factory
+
+  def self.use_relative_model_naming?
+    true
+  end
+
+  def self.table_name_prefix
+    'factory_'
+  end
+
+end
--- lib/rails_factory.rb
+++ lib/rails_factory.rb
@@ -1,11 +1,3 @@
 require 'rails_factory/version'
 require 'rails_factory/engine'
 require 'rails_factory/config'
-
-module Factory
-
-  def self.use_relative_model_naming?
-    true
-  end
-
-end
