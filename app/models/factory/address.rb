67d078744ef1fe27679fb578acbe294e82946c05
MingyuanQin <mingyuan0715@foxmail.com>
2021-05-29 14:10:09 +0800

    moved unused

--- app/models/factory/address.rb
+++ app/models/factory/address.rb
@@ -1,10 +1,6 @@
 module Factory
   class Address < ApplicationRecord
     self.table_name = 'profiled_addresses'
-<<<<<<< HEAD
-    include Profiled::Model::Address if defined? RailsProfile
-=======
->>>>>>> d588c0eb2e32626c3482c7725f3bdd919cdc0641
     include Model::Address
   end
 end
