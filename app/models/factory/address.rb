407936423e5cc66d608e1b339ab241834dbd5d85
MingyuanQin <mingyuan0715@foxmail.com>
2021-05-29 14:05:24 +0800

    rename

--- app/models/factory/address.rb
+++ app/models/factory/address.rb
@@ -1,5 +1,6 @@
 module Factory
   class Address < ApplicationRecord
+    self.table_name = 'profiled_addresses'
     include Profiled::Model::Address if defined? RailsProfile
     include Model::Address
   end
