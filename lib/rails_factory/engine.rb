37e41a8b61ecda1d08e10b7f0b1659f9cc1a2c18
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-29 19:53:09 +0800

    factory

--- lib/rails_factory/engine.rb
+++ lib/rails_factory/engine.rb
@@ -1,17 +1,19 @@
 require 'rails_com'
-class RailsFactory::Engine < ::Rails::Engine
+module RailsFactory
+  class Engine < ::Rails::Engine
 
-  config.generators do |g|
-    g.rails = {
-      assets: false,
-      stylesheets: false,
-      helper: false
-    }
-    g.test_unit = {
-      fixture: true,
-      fixture_replacement: :factory_bot
-    }
-    g.templates.unshift File.expand_path('lib/templates', RailsCom::Engine.root)
-  end
+    config.generators do |g|
+      g.rails = {
+        assets: false,
+        stylesheets: false,
+        helper: false
+      }
+      g.test_unit = {
+        fixture: true,
+        fixture_replacement: :factory_bot
+      }
+      g.templates.unshift File.expand_path('lib/templates', RailsCom::Engine.root)
+    end
 
-end # :nodoc:
+  end # :nodoc:
+end
