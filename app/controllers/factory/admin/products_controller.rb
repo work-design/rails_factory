7d2a185d38a5ce3bc7db4bd85ab2d2bd74c90918
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-29 09:06:27 +0800

    fix products admin

--- app/controllers/factory/admin/products_controller.rb
+++ app/controllers/factory/admin/products_controller.rb
@@ -1,5 +1,46 @@
 module Factory
   class Admin::ProductsController < Admin::BaseController
-    include Controller::Product
+    before_action :set_product, only: [:show, :edit, :part, :update, :destroy]
+
+    def index
+      q_params = {}
+      q_params.merge! default_params
+
+      @products = Product.includes(:parts, :product_taxon).default_where(q_params).order(id: :desc).page(params[:page])
+    end
+
+    def new
+      @product = Product.new
+      @product.product_taxon = ProductTaxon.default_where(default_params).new
+    end
+
+    def edit
+      @product.product_taxon ||= ProductTaxon.default_where(default_params).first
+    end
+
+    def part
+      @parts = Part.default_where(default_params)
+    end
+
+    private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      p = params.fetch(:product, {}).permit(
+        :name,
+        :description,
+        :qr_prefix,
+        :profit_margin,
+        :logo,
+        :product_taxon_ancestors,
+        part_ids: [],
+        covers: [],
+        images: []
+      )
+      p.merge! default_form_params
+    end
+
   end
 end
