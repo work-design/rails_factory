88db3489b6419cef78918f50d0e3f70fc34e54da
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-20 00:45:17 +0800

    add require login

--- app/controllers/factory/base_controller.rb
+++ app/controllers/factory/base_controller.rb
@@ -1,5 +1,6 @@
 module Factory
   class BaseController < BaseController
+    before_action :require_login if defined? RailsAuth
     include Trade::Controller::Application if defined? RailsTrade
   end
 end
