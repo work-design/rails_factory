86afbb3f5df9b0fdbcb54fdef286bc9a28512517
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-22 11:31:23 +0800

    products

--- app/controllers/factory/base_controller.rb
+++ app/controllers/factory/base_controller.rb
@@ -1,6 +1,5 @@
 module Factory
   class BaseController < BaseController
-    before_action :require_login if defined? RailsAuth
     include Trade::Controller::Application if defined? RailsTrade
   end
 end
