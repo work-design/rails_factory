abfea5e7b22114146525819be02a019cb501761f
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-27 20:06:49 +0800

    support plans

--- app/controllers/factory/produce_plans_controller.rb
+++ app/controllers/factory/produce_plans_controller.rb
@@ -3,12 +3,11 @@ module Factory
 
     def index
       q_params = {
-        produce_on: Date.today
+        'produce_on-gte': Date.today
       }
       q_params.merge! default_params
-      q_params.merge! params.permit('name-like', :name)
 
-      @produce_plans = ProducePlan.default_where(q_params).order(id: :asc).page(params[:page])
+      @produce_plans = ProducePlan.default_where(q_params).order(id: :asc).group_by(&:produce_on)
     end
 
     def show
