1c4ca15dfac7aeb0b08a895f3b1b25dfc60b73ab
qinmingyuan <mingyuan0715@foxmail.com>
2021-10-28 19:26:23 +0800

    add item

--- app/controllers/factory/admin/production_items_controller.rb
+++ app/controllers/factory/admin/production_items_controller.rb
@@ -2,6 +2,7 @@ module Factory
   class Admin::ProductionItemsController < Admin::BaseController
     before_action :set_production
     before_action :set_production_item, only: [:show, :edit, :update, :destroy]
+    before_action :set_new_production_item, only: [:index, :new, :create]
 
     def index
       q_params = {}
@@ -14,22 +15,14 @@ module Factory
       @production_items = @production.production_items.default_where(q_params).page(params[:page])
     end
 
-    def new
-      @production_item = @production_plan.production_items.build
-    end
-
-    def create
-      @production_item = @production_plan.production_items.build(production_item_params)
-
-      unless @production_item.save
-        render :new, locals: { model: @production_item }, status: :unprocessable_entity
-      end
-    end
-
     private
     def set_production
       @production = Production.find params[:production_id]
       @production_plan = ProductionPlan.find params[:production_plan_id]
+    end
+
+    def set_new_production_item
+      @production_item = @production_plan.production_items.build(production_item_params)
     end
 
     def set_production_item
