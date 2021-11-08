5d66dace805081d43bb10a2bda7fd0e2530fca21
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-09 00:37:18 +0800

    prouce on

--- app/controllers/factory/admin/scenes_controller.rb
+++ app/controllers/factory/admin/scenes_controller.rb
@@ -1,3 +1,13 @@
-class Factory::Admin::ScenesController < Factory::Admin::BaseController
+module Factory
+  class Admin::ScenesController < Admin::BaseController
 
+    def index
+      q_params = {}
+      q_params.merge! default_params
+      q_params.merge! params.permit(:name)
+
+      @scenes = Scene.default_where(q_params).order(id: :asc).page(params[:page])
+    end
+
+  end
 end
