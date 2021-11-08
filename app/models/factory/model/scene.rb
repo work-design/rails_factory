6a35168d43bde4f5161091f711dd1049fc9b0875
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-09 01:57:32 +0800

    factory

--- app/models/factory/model/scene.rb
+++ app/models/factory/model/scene.rb
@@ -10,6 +10,7 @@ module Factory
       attribute :book_finish_at, :time
       attribute :deliver_start_at, :time
       attribute :deliver_finish_at, :time
+      attribute :specialty, :boolean, default: false
 
       belongs_to :organ, class_name: 'Org::Organ', optional: true
 
@@ -17,6 +18,14 @@ module Factory
 
       has_many :produce_plans, dependent: :nullify
       has_many :product_taxons, dependent: :nullify
+
+      scope :specialty, -> { where(specialty: true) }
+
+      after_update :set_specialty, if: -> { specialty? && saved_change_to_specialty? }
+    end
+
+    def set_specialty
+      self.class.where.not(id: self.id).where(organ_id: self.organ_id).update_all(specialty: false)
     end
 
   end
