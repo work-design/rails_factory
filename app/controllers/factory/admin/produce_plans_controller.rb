58a4dda11364fb72d571ed22327dd12a8ae80b8e
qinmingyuan <mingyuan0715@foxmail.com>
2021-11-22 09:52:10 +0800

    prodcue one

--- app/controllers/factory/admin/produce_plans_controller.rb
+++ app/controllers/factory/admin/produce_plans_controller.rb
@@ -9,7 +9,7 @@ module Factory
       q_params.merge! default_params
       q_params.merge! params.permit(:title)
 
-      @produce_plans = @scene.produce_plans.default_where(q_params).order(id: :desc).page(params[:page])
+      @produce_plans = @scene.produce_plans.default_where(q_params).order(produce_on: :desc).page(params[:page])
     end
 
     def products
