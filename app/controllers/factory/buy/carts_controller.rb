974005664512a0dea626216ed7ba0284ddaa033a
qinmingyuan <mingyuan0715@foxmail.com>
2022-01-09 13:24:08 +0800

    carts

--- app/controllers/factory/buy/carts_controller.rb
+++ app/controllers/factory/buy/carts_controller.rb
@@ -5,30 +5,20 @@ module Factory
       @organ_ids = current_organ.member_carts.order(organ_id: :asc).select(:organ_id).distinct.page(params[:page])
 
       @organs = Org::Organ.find @organ_ids.pluck(:organ_id)
-
     end
 
     def list
-      @carts = current_organ.member_carts.where(organ_id: params[:organ_id])
+      @cart_member_ids = current_organ.member_carts.where(organ_id: params[:organ_id]).pluck(:member_id)
       @members = current_organ.members.page(params[:page])
+
+      (@members.pluck(:id) - @cart_member_ids).each do |member_id|
+        init_cart(member_id, current_organ.id)
+      end
     end
 
     private
-    def xx
-      q_params = {
-        'book_finish_at-gt': Time.current
-      }
-      q_params.merge! default_params
-
-      @produce_plans = Factory::ProducePlan.default_where(q_params).includes(:scene).order(produce_on: :asc).group_by(&:produce_on)
-      @produce_on = @produce_plans.keys[0]
-      if @produce_on
-        @produce_plans = @produce_plans[@produce_on]
-        @produce_plan = @produce_plans.find(&->(i){ i.scene == Factory::Scene.default_where(default_params).specialty.take })
-      else
-        @produce_plans = Factory::ProducePlan.none
-        @produce_on = Date.today
-      end
+    def init_cart(member_id, organ_id)
+      current_organ.member_carts.create(member_id: member_id, organ_id: organ_id)
     end
 
   end
