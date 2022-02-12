3afbb83d2cacc4b1fb8d16b61dc5fe89ca9002b6
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-12 12:51:57 +0800

    add direct to trade item

--- app/controllers/factory/buy/carts_controller.rb
+++ app/controllers/factory/buy/carts_controller.rb
@@ -9,11 +9,7 @@ module Factory
     end
 
     def list
-      @cart_member_ids = current_organ.member_carts.where(organ_id: params[:organ_id]).pluck(:member_id)
       @members = current_organ.members.order(id: :asc).page(params[:page])
-      (@members.pluck(:id) - @cart_member_ids).each do |member_id|
-        init_cart(member_id, params[:organ_id])
-      end
 
       @carts = current_organ.member_carts.includes(:member, :trade_items).where(organ_id: params[:organ_id]).order(member_id: :asc).page(params[:page])
       if params[:produce_plan_id]
@@ -23,14 +19,6 @@ module Factory
       else
         render 'list'
       end
-    end
-
-    def add
-      trade_item = @cart.get_trade_item(params[:good_type], params[:good_id], params[:number], params[:produce_plan_id])
-      trade_item.save
-
-      @trade_items = @cart.trade_items.page(params[:page])
-      @checked_ids = @cart.trade_items.checked.pluck(:id)
     end
 
     private
