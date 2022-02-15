f43fd972abffbb40c6e6b9d07944db74a93ef630
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-15 23:57:28 +0800

    inhe form productions

--- app/controllers/factory/buy/productions_controller.rb
+++ app/controllers/factory/buy/productions_controller.rb
@@ -1,5 +1,5 @@
 module Factory
-  class Buy::ProductionsController < Buy::BaseController
+  class Buy::ProductionsController < ProductionsController
     before_action :set_product_taxon
     before_action :set_produce_plans, only: [:index, :plan]
     before_action :set_product_taxons, only: [:index]
