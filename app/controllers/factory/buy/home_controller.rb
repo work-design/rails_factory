a87e9ec41362084b1eda1aff29d89c0716478f5c
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-22 23:35:36 +0800

    got realy exist scene ids

--- app/controllers/factory/buy/home_controller.rb
+++ app/controllers/factory/buy/home_controller.rb
@@ -3,6 +3,9 @@ module Factory
 
     def index
       @produce_on = Date.today
+
+      ids = ProducePlan.where(produce_on: @produce_on, organ_id: current_organ.provider_ids).select(:scene_id).distinct.pluck(:scene_id)
+      @scenes = Scene.where(id: ids)
     end
 
   end
