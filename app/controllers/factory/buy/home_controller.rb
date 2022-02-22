d63f8fb972b2c2e98b10b33ca648df105b1003f6
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-23 00:04:23 +0800

    date today

--- app/controllers/factory/buy/home_controller.rb
+++ app/controllers/factory/buy/home_controller.rb
@@ -2,7 +2,7 @@ module Factory
   class Buy::HomeController < Buy::BaseController
 
     def index
-      @produce_on = Date.today + 1
+      @produce_on = ProducePlan.where(organ_id: current_organ.provider_ids).effective.order(produce_on: :asc).first&.produce_on || Date.today
 
       ids = ProducePlan.where(produce_on: @produce_on, organ_id: current_organ.provider_ids).select(:scene_id).distinct.pluck(:scene_id)
       @scenes = Scene.where(id: ids)
