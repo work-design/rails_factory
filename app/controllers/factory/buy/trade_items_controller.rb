e234c45be81a395eee5fd5aa8c675051e85574dd
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-23 11:34:34 +0800

    add to agent

--- app/controllers/factory/buy/trade_items_controller.rb
+++ app/controllers/factory/buy/trade_items_controller.rb
@@ -1,5 +1,6 @@
 module Factory
   class Buy::TradeItemsController < Buy::BaseController
+    before_action :set_agent
     before_action :set_trade_item, only: [:show, :toggle, :edit, :update, :destroy]
 
     def index
@@ -7,18 +8,6 @@ module Factory
       q_params.merge! params.permit(:good_id)
 
       @trade_items = current_organ.member_ordered_trade_items.includes(:organ).default_where(q_params).page(params[:page])
-    end
-
-    def new
-      @trade_item = TradeItem.new
-    end
-
-    def create
-      @trade_item = TradeItem.new(trade_item_params)
-
-      unless @trade_item.save
-        render :new, locals: { model: @trade_item }, status: :unprocessable_entity
-      end
     end
 
     def toggle
@@ -34,6 +23,16 @@ module Factory
     private
     def set_address
       @address = current_user.principal_addresses.find params[:principal_address_id]
+    end
+
+    def set_agent
+      @agent = current_member
+    end
+
+    def set_new_trade_item
+      options = params.permit(:good_type, :good_id, :member_id, :number, :produce_on, :scene_id)
+
+      @trade_item = @agent.get_trade_item(**options.to_h.symbolize_keys)
     end
 
     def set_trade_item
