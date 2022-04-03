67e87e13732777b517e2992bc91f7e9f22de2067
qinmingyuan <mingyuan0715@foxmail.com>
2022-04-03 20:31:24 +0800

    select

--- app/models/factory/model/product_part_taxon.rb
+++ app/models/factory/model/product_part_taxon.rb
@@ -15,5 +15,29 @@ module Factory
       has_taxons :part_taxon
     end
 
+    def select_str
+      if max_select > min_select
+        '可选'
+      else
+        '必选'
+      end
+    end
+
+    def select_range
+      if max_select == min_select
+        "#{max_select}"
+      else
+        "#{min_select} - #{max_select}"
+      end
+    end
+
+    def disabled?(part)
+      if parts.size <= min_select
+        true
+      else
+        false
+      end
+    end
+
   end
 end
