2bfb0ada34f0ec13b8c11b3a34431c0b77ce8cc2
qinmingyuan <mingyuan0715@foxmail.com>
2022-04-04 00:43:21 +0800

    part plans

--- app/models/factory/model/part_item.rb
+++ app/models/factory/model/part_item.rb
@@ -16,18 +16,14 @@ module Factory
 
       belongs_to :part
       belongs_to :product_item, optional: true
-      has_one_attached :qr_file
 
       after_initialize if: :new_record? do
         self.qr_code ||= UidHelper.nsec_uuid self.part&.qr_prefix
       end
-      before_save :sync_qrcode, if: -> { qr_code_changed? }
     end
 
-    def sync_qrcode
-      file = QrcodeHelper.code_file(qr_code)
-      file.rewind
-      self.qr_file.attach io: file, filename: qr_code
+    def qrcode_url
+      QrcodeHelper.data_url(qr_code)
     end
 
   end
