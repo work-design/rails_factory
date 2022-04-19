74966674e6145f0cd48afa639585b201302f226c
qinmingyuan <mingyuan0715@foxmail.com>
2022-04-20 02:29:53 +0800

    factory

--- app/controllers/factory/my/trade_items_controller.rb
+++ app/controllers/factory/my/trade_items_controller.rb
@@ -1,10 +1,15 @@
 module Factory
   class My::TradeItemsController < Trade::My::TradeItemsController
     before_action :set_production
+    before_action :set_card_templates, only: [:create, :update]
 
     private
     def set_production
       @production = @trade_item.good
+    end
+
+    def set_card_templates
+      @card_templates = Trade::CardTemplate.default_where(default_params)
     end
   end
 end
