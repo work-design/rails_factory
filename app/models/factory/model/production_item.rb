63a5fdaac0607ab4001933b4c9a82d144490563b
qinmingyuan <mingyuan0715@foxmail.com>
2022-05-21 19:56:09 +0800

    not use ing for state

--- app/models/factory/model/production_item.rb
+++ app/models/factory/model/production_item.rb
@@ -3,17 +3,16 @@ module Factory
     extend ActiveSupport::Concern
 
     included do
-      attribute :state, :string, default: 'producing'
       attribute :qr_code, :string
-      attribute :produced_at, :datetime
+      attribute :produced_at, :datetime, default: -> { Time.current }
 
       belongs_to :production
       belongs_to :production_plan, counter_cache: true
       has_many :part_items
 
       enum state: {
-        producing: 'producing',
-        receiving: 'receiving',
+        produced: 'produced',
+        received: 'received',
         warehouse_in: 'warehouse_in',
         warehouse_out: 'warehouse_out',
         used: 'used'
@@ -22,7 +21,6 @@ module Factory
       after_initialize if: :new_record? do
         self.production = self.production_plan.production if production_plan
         self.qr_code ||= UidHelper.nsec_uuid self.production&.qr_code
-        self.produced_at ||= Time.current
       end
     end
 
