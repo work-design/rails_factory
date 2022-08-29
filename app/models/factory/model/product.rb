56df477b1071e36fd2663528b602cd64b4fa5067
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-30 00:03:38 +0800

    fix err

--- app/models/factory/model/product.rb
+++ app/models/factory/model/product.rb
@@ -25,8 +25,8 @@ module Factory
       belongs_to :unifier, optional: true
       belongs_to :product_taxon, counter_cache: true, optional: true
       belongs_to :brand, counter_cache: true, optional: true
-      belongs_to :upstream_product, optional: true
-      belongs_to :source_product, optional: true  # 上游产品
+      belongs_to :upstream_product, class_name: name, optional: true
+      belongs_to :source_product, class_name: name, optional: true  # 上游产品
 
       has_one :production, -> { where(default: true) }
       has_many :productions, dependent: :destroy_async
--- app/models/factory/model/production_part.rb
+++ app/models/factory/model/production_part.rb
@@ -4,7 +4,7 @@ module Factory
 
     included do
       belongs_to :production
-      belongs_to :part, class_name: 'Product'
+      belongs_to :part, class_name: 'Production'
 
       after_commit :sync_to_production, on: [:create, :destroy]
     end
