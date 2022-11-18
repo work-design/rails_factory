d9afc6da834a557d33ff46d78b6e45ed547e71d8
qinmingyuan <mingyuan0715@foxmail.com>
2022-11-18 11:28:36 +0800

    by order

--- app/controllers/factory/admin/factory_taxons_controller.rb
+++ app/controllers/factory/admin/factory_taxons_controller.rb
@@ -2,15 +2,7 @@ module Factory
   class Admin::FactoryTaxonsController < Admin::BaseController
     before_action :set_factory_taxon, only: [:show, :import, :productions, :edit, :update, :destroy]
 
-    def import
-      @products = @factory_taxon.products.page(params[:page])
 
-      q_params = {}
-      q_params.merge! 'part.organ_id': current_organ.id if current_organ
-
-      product_ids = @products.pluck(:id)
-      @select_ids = PartProvider.default_where(q_params).where(product_id: product_ids).pluck(:product_id)
-    end
 
     private
     def set_factory_taxon
