3f0a3a9f73c22e788ab33cd97a50bd49af5c09ed
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-12 23:44:27 +0800

    requeire

--- app/controllers/factory/in/base_controller.rb
+++ app/controllers/factory/in/base_controller.rb
@@ -1,11 +1,6 @@
 module Factory
   class In::BaseController < InController
-
-    def require_member(url: url_for(controller: 'factory/in/home', action: 'organs'))
-      return if current_member
-
-      redirect_to url
-    end
+    include Org::Controller::In
 
   end
 end
