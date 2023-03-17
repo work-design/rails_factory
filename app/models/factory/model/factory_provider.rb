795778d87c691fd82721bd722ecafc505a90c3ab
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-17 19:40:46 +0800

    product taxons

--- app/models/factory/model/factory_provider.rb
+++ app/models/factory/model/factory_provider.rb
@@ -3,7 +3,7 @@ module Factory
     extend ActiveSupport::Concern
 
     included do
-      belongs_to :factory_taxon
+      belongs_to :factory_taxon, counter_cache: true
       belongs_to :provider, class_name: 'Org::Organ'
 
       validates :provider_id, uniqueness: { scope: :factory_taxon_id }
