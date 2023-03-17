202dfa3d6dd50489bdaead278d569ef0a3ad2ae1
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-17 17:42:51 +0800

    roles

--- app/controllers/factory/in/home_controller.rb
+++ app/controllers/factory/in/home_controller.rb
@@ -1,12 +1,23 @@
 module Factory
   class In::HomeController < In::BaseController
     skip_before_action :require_member, only: [:organs]
+    before_action :set_roles, only: [:organs]
     def index
     end
 
     def organs
       @organs = current_account.organs.includes(:organ_domains)
     end
+
+    private
+    def set_roles
+      if params[:role_id].present?
+        @roles = Roled::OrganRole.visible.where(id: params[:role_id])
+      else
+        @roles = Roled::OrganRole.visible
+      end
+    end
+
 
   end
 end
