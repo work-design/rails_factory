76b4f6cb4a4bc4446469f5b54cac45f8f6d20cea
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-24 11:17:10 +0800

    factory

--- app/controllers/factory/in/produce_plans_controller.rb
+++ app/controllers/factory/in/produce_plans_controller.rb
@@ -7,7 +7,7 @@ module Factory
       @produce_on = ProducePlan.where(organ_id: current_organ.provider_ids).effective.order(produce_on: :asc).first&.produce_on || Date.today
 
       ids = ProducePlan.where(produce_on: @produce_on, organ_id: current_organ.provider_ids).select(:scene_id).distinct.pluck(:scene_id)
-      @scenes = Scene.where(id: ids)
+      @scenes = Scene.where(id: ids).order(id: :asc)
     end
 
     def index
