2cb72dd837a6462b79320e566aab245253618738
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-29 16:26:34 +0800

    sync part taxon

--- app/models/factory/model/production_part.rb
+++ app/models/factory/model/production_part.rb
@@ -5,8 +5,14 @@ module Factory
     included do
       belongs_to :production
       belongs_to :part, class_name: 'Production'
+      belongs_to :part_taxon, class_name: 'ProductTaxon'
 
+      before_validation :sync_part_taxon, if: -> { part_id_changed? }
       after_commit :sync_to_production, on: [:create, :destroy]
+    end
+
+    def sync_part_taxon
+      self.part_taxon = part.product_taxon
     end
 
     def sync_to_production
