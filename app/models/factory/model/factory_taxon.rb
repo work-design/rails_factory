fb1641b21212d36cfcf9e0ac5b4d562a6a5078c5
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-03 09:52:27 +0800

    products

--- app/models/factory/model/factory_taxon.rb
+++ app/models/factory/model/factory_taxon.rb
@@ -11,7 +11,7 @@ module Factory
 
       has_many :factory_providers, dependent: :delete_all
       has_many :providers, through: :factory_providers
-      has_many :products, ->(o) { where(organ_id: o.provider_ids) }
+      has_many :products
       has_many :productions, through: :products, source: :productions
       has_many :product_taxons
 
