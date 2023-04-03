3aa76db584e5681fc6dfcb9c826e1d88f810a5ac
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-03 09:57:26 +0800

    show production price

--- app/controllers/factory/productions_controller.rb
+++ app/controllers/factory/productions_controller.rb
@@ -18,10 +18,10 @@ module Factory
         if @produce_plan.expired?
           render 'expired'
         else
-          @productions = @produce_plan.productions.includes(:parts, :product).default.default_where(q_params).order(id: :asc).page(params[:page]).per(10)
+          @productions = @produce_plan.productions.includes(:parts, :product).default.default_where(q_params).order(id: :asc).page(params[:page]).per(params[:per])
         end
       else
-        @productions = Production.includes(:parts, :product).enabled.default.default_where(q_params).order(id: :asc).page(params[:page]).per(10)
+        @productions = Production.includes(:parts, :product).enabled.default.default_where(q_params).order(id: :asc).page(params[:page]).per(params[:per])
       end
     end
 
