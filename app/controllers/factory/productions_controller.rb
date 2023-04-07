b748852db56fbf35725c8cdc83ce07c42e755d02
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-07 20:06:17 +0800

    gem

--- app/controllers/factory/productions_controller.rb
+++ app/controllers/factory/productions_controller.rb
@@ -104,9 +104,9 @@ module Factory
     def set_cart
       options = {}
       options.merge! default_form_params
-      options.merge! user_id: current_user&.id, member_id: nil
 
       if current_user
+        options.merge! user_id: current_user.id, member_id: nil
         @cart = Trade::Cart.where(options).find_or_create_by(good_type: 'Factory::Production', aim: 'use')
       end
     end
