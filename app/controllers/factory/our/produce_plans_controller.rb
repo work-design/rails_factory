b53cd26fc5b9025c684a74377963cdafebac0d9b
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-13 19:13:35 +0800

    add logoc

--- app/controllers/factory/our/produce_plans_controller.rb
+++ app/controllers/factory/our/produce_plans_controller.rb
@@ -5,9 +5,9 @@ module Factory
       q_params = {
         'book_finish_at-gt': Time.current
       }
-      q_params.merge! organ_id: current_organ.provider_ids.uniq
+      q_params.merge! default_params
 
-      @produce_plans = ProducePlan.includes(:scene).default_where(q_params).order(produce_on: :asc).group_by(&:produce_on)
+      @produce_plans = ProducePlan.includes(scene: { logo_attachment: :blob }).default_where(q_params).order(produce_on: :asc).group_by(&:produce_on)
     end
 
     def show
