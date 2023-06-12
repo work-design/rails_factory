4d68e0ba13233a2cbee1e9b45d8c385653914cdc
qinmingyuan <mingyuan0715@foxmail.com>
2023-06-12 17:50:06 +0800

    item extra

--- app/models/factory/production.rb
+++ app/models/factory/production.rb
@@ -3,5 +3,9 @@ module Factory
     include Model::Production
     include Trade::Ext::Good
     include Trade::Ext::Rentable
+
+    def item_extra
+      attributes.slice('product_taxon_id')
+    end
   end
 end
