3b3bdab9dc0e828c2a4f56be8b040b9983d7cafa
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-10 16:41:22 +0800

    update provider

--- app/controllers/factory/in/items_controller.rb
+++ app/controllers/factory/in/items_controller.rb
@@ -1,9 +1,17 @@
 module Factory
   class In::ItemsController < Trade::In::ItemsController
-    before_action :set_item, only: [:show, :edit_assign]
+    before_action :set_item, only: [:show, :edit_assign, :update]
+    before_action :set_cart_item, only: []
 
     def edit_assign
       @providers = current_organ.providers
+    end
+
+    private
+    def item_params
+      params.fetch(:item, {}).permit(
+        :organ_id
+      )
     end
 
   end
