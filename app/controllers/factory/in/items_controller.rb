6b070dcca52b56682063e9bf52199487d1ed0fb1
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-27 18:33:49 +0800

    add items

--- app/controllers/factory/in/items_controller.rb
+++ app/controllers/factory/in/items_controller.rb
@@ -1,11 +1,16 @@
 module Factory
   class In::ItemsController < Trade::In::ItemsController
-    before_action :set_item, only: [:show, :edit_assign, :update]
-    before_action :set_cart_item, only: []
+    before_action :set_item, only: [:show, :edit_assign, :update_assign]
+    before_action :set_cart_item, only: [:update, :destroy]
 
     def edit_assign
       upstream_organ_ids = @item.purchase.upstream_provides.pluck(:provider_id)
       @providers = current_organ.providers.where.not(id: upstream_organ_ids)
+    end
+
+    def update_assign
+      @item.assign_attribute item_params
+      @item.save
     end
 
     private
