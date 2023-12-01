16cb0a066b70db775ad393dd1451147ab543bea3
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-01 19:07:28 +0800

    layout

--- app/controllers/factory/controller/in.rb
+++ app/controllers/factory/controller/in.rb
@@ -3,7 +3,7 @@ module Factory
     extend ActiveSupport::Concern
 
     included do
-      layout -> { turbo_frame_body? ? 'frame/body' : 'in' }
+      layout -> { turbo_frame_body? ? "frame/#{proper_layout}" : 'in' }
     end
 
     def set_scenes
