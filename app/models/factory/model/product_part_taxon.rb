d1521d6082420b89b2c27e3e5785664f63918b46
qinmingyuan <mingyuan0715@foxmail.com>
2023-12-11 11:06:09 +0800

    simple codes

--- app/models/factory/model/product_part_taxon.rb
+++ app/models/factory/model/product_part_taxon.rb
@@ -10,18 +10,13 @@ module Factory
 
       belongs_to :product
       belongs_to :part_taxon, class_name: 'ProductTaxon'
-      has_many :product_parts, ->(o){ where(part_taxon_id: o.part_taxon_id) }, primary_key: :product_id, foreign_key: :product_id
+      has_many :product_parts, ->(o){ where(part_taxon_id: o.part_taxon_id) }, primary_key: :product_id, foreign_key: :product_id, inverse_of: :product_part_taxon
       has_many :parts, through: :product_parts
 
       validates :min_select, numericality: { only_integer: true, less_than_or_equal_to: -> (o) { o.max_select } }
       #validates :max_select, numericality: { only_integer: true, less_than_or_equal_to: -> (o) { o.product_parts_count } }
 
       before_validation :sync_name, if: -> { product_id_changed? }
-      after_create_commit :reset_product_parts_counter
-    end
-
-    def reset_product_parts_counter
-      self.class.reset_counters(self.id, :product_parts)
     end
 
     def sync_name
