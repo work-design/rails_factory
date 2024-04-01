7a98a72b1eaf0ae0eaa7d2043ae6e61d5c2dd390
qinmingyuan <mingyuan0715@foxmail.com>
2024-04-01 14:19:39 +0800

    form fields

--- app/models/factory/model/provide.rb
+++ app/models/factory/model/provide.rb
@@ -3,12 +3,14 @@ module Factory
     extend ActiveSupport::Concern
 
     included do
+      attribute :name, :string
+
       belongs_to :organ, class_name: 'Org::Organ'
-      belongs_to :provider, class_name: 'Org::Organ'
+      belongs_to :provider, class_name: 'Org::Organ', optional: true
 
       belongs_to :product_taxon, counter_cache: true, optional: true
 
-      validates :provider_id, uniqueness: { scope: :product_taxon_id }
+      validates :provider_id, uniqueness: { scope: :product_taxon_id }, allow_blank: true
 
       after_initialize :sync_organ, if: :new_record?
     end
