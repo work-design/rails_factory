6a0e31cd6da40b94dd1958b6e6bd6bcccd4299f1
qinmingyuan <mingyuan0715@foxmail.com>
2024-05-11 21:09:46 +0800

    lst

--- app/controllers/factory/admin/product_parts_controller.rb
+++ app/controllers/factory/admin/product_parts_controller.rb
@@ -1,15 +1,20 @@
 module Factory
   class Admin::ProductPartsController < Admin::BaseController
     before_action :set_product
+    before_action :set_product_part_taxon
     before_action :set_product_part, only: [:show, :edit, :update, :destroy]
 
     def index
-      @product_parts = @product.product_parts.order(part_id: :asc).page(params[:page])
+      @product_parts = @product_part_taxon.product_parts.order(part_id: :asc).page(params[:page])
     end
 
     private
     def set_product
       @product = Product.find params[:product_id]
+    end
+
+    def set_product_part_taxon
+      @product_part_taxon = @product.product_part_taxons.find params[:product_part_taxon_id]
     end
 
     def set_new_product_part
