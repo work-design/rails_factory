96eec58f1e8649e9ddd0f2b2431c540383e0183d
qinmingyuan <mingyuan0715@foxmail.com>
2024-07-02 17:59:28 +0800

    fix error

--- app/models/factory/product.rb
+++ app/models/factory/product.rb
@@ -2,5 +2,6 @@ module Factory
   class Product < ApplicationRecord
     include Model::Product
     include Auditor::Ext::Discard if defined? RailsAudit
+    include Qingflow::Ext::Related if defined? RailsQingflow
   end
 end
