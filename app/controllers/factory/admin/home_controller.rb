d8e1a4bcb23f3c6bd103f58c28b036149938f9f9
qinmingyuan <mingyuan0715@foxmail.com>
2024-07-11 15:16:54 +0800

    admin home

--- app/controllers/factory/admin/home_controller.rb
+++ app/controllers/factory/admin/home_controller.rb
@@ -2,6 +2,8 @@ module Factory
   class Admin::HomeController < Admin::BaseController
 
     def index
+      @taxon_count = ProductTaxon.default_where(default_params).count
+      @scene_count = Scene.default_where(default_params).count
     end
 
   end
