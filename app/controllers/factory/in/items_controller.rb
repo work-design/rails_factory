00eb70e2da4ace236363d7480c972d0703f01386
qinmingyuan <279378275@qq.com>
2024-08-04 11:33:37 +0800

    simple buy

--- app/controllers/factory/in/items_controller.rb
+++ app/controllers/factory/in/items_controller.rb
@@ -1,6 +1,8 @@
 module Factory
   class In::ItemsController < Trade::In::ItemsController
-    before_action :set_cart, :set_item, only: [:show, :edit_assign, :update_assign]
+    before_action :set_cart, only: [:create, :show, :edit_assign, :update_assign]
+    before_action :set_item, only: [:show, :edit_assign, :update_assign]
+    before_action :set_new_item, only: [:create]
 
     def edit_assign
       upstream_organ_ids = @item.purchase.production_provides.pluck(:provider_id)
@@ -13,10 +15,6 @@ module Factory
     end
 
     private
-    def set_cart
-      @cart = Trade::Cart.get_cart(params, member_organ_id: current_organ.id)
-    end
-
     def organ_item_params
       params.fetch(:item, {}).permit(
         :organ_id
