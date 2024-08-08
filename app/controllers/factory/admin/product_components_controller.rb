9ade191f9b53efff0caa83c58d60ee49921b5dd0
qinmingyuan <279378275@qq.com>
2024-08-08 20:43:48 +0800

    product prosut

--- app/controllers/factory/admin/product_components_controller.rb
+++ app/controllers/factory/admin/product_components_controller.rb
@@ -1,12 +1,12 @@
 module Factory
   class Admin::ProductComponentsController < Admin::BaseController
-    before_action :set_product, if: -> { params[:product_id].present? }
-    before_action :set_taxon_component, only: [:show, :edit, :update, :destroy]
-    before_action :set_new_taxon_component, only: [:new, :create]
+    before_action :set_product
+    before_action :set_product_component, only: [:show, :edit, :update, :destroy]
+    before_action :set_new_product_component, only: [:new, :create]
     before_action :set_part_taxons, only: [:new, :create, :edit, :update]
 
     def index
-      @taxon_components = @product.taxon_components.includes(:part_taxon, product_parts: :part).order(part_taxon_id: :asc).page(params[:page])
+      @product_components = @product.product_components.includes(:part_taxon, product_parts: :part).order(part_taxon_id: :asc).page(params[:page])
     end
 
     private
@@ -14,20 +14,20 @@ module Factory
       @product = Product.find params[:product_id]
     end
 
-    def set_taxon_component
-      @taxon_component = ProductPartTaxon.find(params[:id])
+    def set_product_component
+      @product_component = @product.product_components.find(params[:id])
     end
 
-    def set_new_taxon_component
-      @taxon_component = @product.taxon_components.build(taxon_component_params)
+    def set_new_product_component
+      @product_component = @product.product_components.build(product_component_params)
     end
 
     def set_part_taxons
       @part_taxons = Taxon.default_where(default_params).order(id: :asc)
     end
 
-    def taxon_component_params
-      params.fetch(:taxon_component, {}).permit(
+    def product_component_params
+      params.fetch(:product_component, {}).permit(
         :min_select,
         :max_select,
         :part_taxon_id,
