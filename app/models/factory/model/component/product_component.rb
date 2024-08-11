c5e82e1d5069475b97956dea46a2440c89aa2a3a
qinmingyuan <279378275@qq.com>
2024-08-11 23:47:37 +0800

    share files

--- app/models/factory/model/component/product_component.rb
+++ app/models/factory/model/component/product_component.rb
@@ -24,38 +24,7 @@ module Factory
       self.taxon_id = product.taxon_id
     end
 
-    def select_str
-      if max_select == min_select && product_parts_count > max_select.to_i
-        "#{product_parts_count} 选 #{max_select}"
-      elsif max_select == min_select && product_parts_count == max_select
-        "必选 #{max_select}"
-      elsif max_select.to_i > min_select.to_i
-        "可选 #{min_select} - #{max_select}"
-      else
-        ''
-      end
-    end
 
-    def only_one?
-      max_select == min_select && max_select == 1 && product_parts_count > max_select
-    end
-
-    def disabled?(production_part_ids, part_id)
-      select_ids = part_ids & production_part_ids
-
-      return select_ids.all?(part_id) if only_one?
-      return true if select_ids.size == min_select && select_ids.include?(part_id)
-      return true if select_ids.size == max_select && select_ids.exclude?(part_id)
-
-      false
-    end
-
-    def cancelable?(production_part_ids, part_id)
-      select_ids = part_ids & production_part_ids
-
-      return false if only_one?
-      select_ids.include?(part_id)
-    end
 
   end
 end
