5a667c072dbdbfbae3f157860a50c25da9fde576
qinmingyuan <279378275@qq.com>
2024-08-13 19:49:14 +0800

    taxon components

--- app/controllers/factory/admin/taxon_components_controller.rb
+++ app/controllers/factory/admin/taxon_components_controller.rb
@@ -4,7 +4,7 @@ module Factory
     before_action :set_new_taxon_component, only: [:new, :create]
 
     def index
-      @taxon_components = @taxon.taxon_components.includes(:part_taxon, taxon_parts: :part).order(part_taxon_id: :asc).page(params[:page])
+      @taxon_components = @taxon.taxon_components.includes(:part_taxon, component_parts: :part).order(part_taxon_id: :asc).page(params[:page])
     end
 
     private
@@ -14,6 +14,16 @@ module Factory
 
     def set_new_taxon_component
       @taxon_component = @taxon.taxon_components.build(taxon_component_params)
+    end
+
+    def taxon_component_params
+      params.fetch(:taxon_component, {}).permit(
+        :min_select,
+        :max_select,
+        :part_taxon_id,
+        :name,
+        part_ids: []
+      )
     end
   end
 end
