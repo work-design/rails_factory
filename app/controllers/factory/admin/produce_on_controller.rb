b7fe3795327ce4f64845a107b95b106121e16cbe
qinmingyuan <279378275@qq.com>
2024-08-18 23:29:04 +0800

    produce on

--- app/controllers/factory/admin/produce_on_controller.rb
+++ app/controllers/factory/admin/produce_on_controller.rb
@@ -10,7 +10,7 @@ module Factory
       }
       q_params.merge! default_params
 
-      @produce_plans = ProducePlan.default_where(q_params).group_by(&->(i){ i.produce_on.to_s }).with_defaults! @produce_ons.to_a.map!(&:to_s).product([[]]).to_h
+      @produce_plans = ProducePlan.default_where(q_params).group_by(&->(i){ i.produce_on.to_s })
     end
 
   end
