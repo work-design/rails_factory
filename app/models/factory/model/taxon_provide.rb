68a4446864261b81ea5819d7c1a49d0ed323d024
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-11 22:34:35 +0800

    taxon

--- app/models/factory/model/taxon_provide.rb
+++ app/models/factory/model/taxon_provide.rb
@@ -14,7 +14,7 @@ module Factory
     end
 
     def automatic_production_provide
-      missing_provides = productions.where.missing(:production_provides).map do |i|
+      missing_provides = taxon.productions.where.missing(:production_provides).map do |i|
         { production_id: i.id, product_id: i.product_id, taxon_id: i.taxon_id, provide_id: provide_id }
       end
 
