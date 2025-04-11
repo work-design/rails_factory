0f60c0a001971c2b95d8c22ac1ac77eb8b4ad1a3
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-11 10:50:27 +0800

    provide cofnig

--- app/models/factory/model/taxon_provide.rb
+++ app/models/factory/model/taxon_provide.rb
@@ -9,17 +9,21 @@ module Factory
 
       belongs_to :provide
       belongs_to :taxon
-    end
 
+      has_many :production_provides, as: :provide_config
+    end
 
     def automatic_production_provide
       missing_provides = productions.where.missing(:production_provides).map do |i|
-        { production_id: i.id, product_id: i.product_id, provide_id: provide_id, source_type: 'ProductProvide', source_id: self.id }
+        { production_id: i.id, product_id: i.product_id, taxon_id: i.taxon_id, provide_id: provide_id }
       end
 
-      taxon.production_provides.insert_all(missing_provides)
+      production_provides.insert_all(missing_provides)
     end
 
+    def automatic_as_default
+      taxon.production_provides.where(provide_config_type: self.class.name).update_all(provide_id: provide_id, provide_config_id: self.id)
+    end
 
   end
 end
