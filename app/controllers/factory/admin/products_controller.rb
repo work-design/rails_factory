e9fcf97e1efeab5ac9d1f76f69e3883d52690b7b
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-12 15:26:00 +0800

    share some resources

--- app/controllers/factory/admin/products_controller.rb
+++ app/controllers/factory/admin/products_controller.rb
@@ -3,7 +3,7 @@ module Factory
     before_action :set_brands, only: [:new, :create, :edit, :update]
     before_action :set_product, only: [:show, :edit, :update, :destroy, :reorder, :actions, :part, :edit_image]
     before_action :set_new_product, only: [:new, :create]
-    before_action :set_taxons, only: [:index, :new, :create, :edit, :update]
+    before_action :set_taxons, only: [:index, :buy, :new, :create, :edit, :update]
     before_action :set_cart, only: [:buy]
 
     def index
